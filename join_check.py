c63c8059502fa7df36c8a3d436746ee42aee05ed
OSINT <polaramkatariya50@gmail.com>
2026-01-12 21:56:55 +0530

    Update join_check.py

--- join_check.py
+++ join_check.py
@@ -2,9 +2,9 @@ from pyrogram.errors import UserNotParticipant
 from config import MUST_JOIN_CHANNELS
 
 async def check_join(client, user_id):
-    for ch in MUST_JOIN_CHANNELS:
+    for channel in MUST_JOIN_CHANNELS:
         try:
-            await client.get_chat_member(ch, user_id)
+            await client.get_chat_member(channel, user_id)
         except UserNotParticipant:
             return False
     return True
