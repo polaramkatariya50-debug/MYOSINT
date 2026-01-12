3269447f6137bd14bbbcd2fbb02c0621cd37e784
OSINT <polaramkatariya50@gmail.com>
2026-01-12 22:04:51 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,23 +1,11 @@
-from pyrogram import Client, filters
-from database import get_user, add_user
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import MUST_JOIN_CHANNELS
 
-@Client.on_message(filters.command("start"))
-async def start(client, message):
-    uid = message.from_user.id
-    ref = None
+buttons = [
+    [InlineKeyboardButton("🔗 JOIN CHANNEL", url=link)]
+    for link in MUST_JOIN_CHANNELS.values()
+]
 
-    if len(message.command) > 1 and message.command[1].startswith("ref_"):
-        ref = int(message.command[1].split("_")[1])
+buttons.append([InlineKeyboardButton("✅ VERIFY", callback_data="verify")])
 
-    if not get_user(uid):
-        add_user(uid, ref)
-
-    kb = InlineKeyboardMarkup([
-        [InlineKeyboardButton("✅ VERIFY", callback_data="verify")]
-    ])
-
-    await message.reply(
-        "👋 Welcome!\n\nJoin all channels & click VERIFY to get 10 credits.",
-        reply_markup=kb
-    )
+kb = InlineKeyboardMarkup(buttons)
