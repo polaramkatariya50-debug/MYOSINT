4d8e10d0b673dc3cc6bae28ae00bc8aea2a9cfea
OSINT <polaramkatariya50@gmail.com>
2026-01-12 21:56:37 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,25 +5,32 @@ client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
 users = db.users
-logs = db.logs
+
 
 def get_user(uid):
     return users.find_one({"_id": uid})
+
 
 def add_user(uid, ref=None):
     users.insert_one({
         "_id": uid,
         "credits": 0,
         "verified": False,
-        "ref_by": ref,
-        "refs": 0
+        "ref_by": ref
     })
 
-def add_credit(uid, amt):
-    users.update_one({"_id": uid}, {"$inc": {"credits": amt}})
 
-def deduct_credit(uid, amt):
-    users.update_one({"_id": uid}, {"$inc": {"credits": -amt}})
+def add_credit(uid, amount):
+    users.update_one({"_id": uid}, {"$inc": {"credits": amount}})
+
+
+def deduct_credit(uid, amount):
+    users.update_one({"_id": uid}, {"$inc": {"credits": -amount}})
+
 
 def set_verified(uid):
-    users.update_one({"_id": uid}, {"$set": {"verified": True}})
+    users.update_one(
+        {"_id": uid},
+        {"$set": {"verified": True}},
+        upsert=True
+    )
