824eed69266346c3d5770ffd272ba7d8f31134eb
OSINT <polaramkatariya50@gmail.com>
2026-01-12 21:46:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8556191807:AAGt7noEjSaRlxZChZPFCorb3DlaxcTO_X0")
-
-API_ID = int(os.getenv("28795512"))
-API_HASH = os.getenv("c17e4eb6d994c9892b8a8b6bfea4042a")
-
-MONGO_URI = os.getenv("mongodb+srv://vishalpandeynimport os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # Telegram Bot
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
@@ -37,23 +27,6 @@ MUST_JOIN_CHANNELS = [
 ]
 
 # Credits
-SEARCH_COST = 2
-NEW_USER_CREDIT = 10
-REF_NEW_USER = 10
-REF_OWNER = 5
-kp:Bal6Y6FZeQeoAoqV@cluster0.dzgwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.getenv("DB_NAME", "vni0x")
-
-OWNER_IDS = {int(x) for x in os.getenv("OWNER_IDS", "").split(",") if x}
-
-MUST_JOIN_CHANNELS = [
-    "whz_G-xn0KdkZWI1",
-    "heroku_club",
-    "NOBITA_SUPPORT",
-    "OsintInformationGroup",
-    "Ah7RcBKx4zQ2YTE1",
-]
-
 SEARCH_COST = 2
 NEW_USER_CREDIT = 10
 REF_NEW_USER = 10
