dd00d87d888cb92cd183f20ed4fa67c836c5432d
OSINT <polaramkatariya50@gmail.com>
2026-01-12 22:24:30 +0530

    Update fampay.py

--- formats/fampay.py
+++ formats/fampay.py
@@ -1,13 +1,21 @@
-def fampay_format(body: str):
-    return f"""
-══════════════  F A M P A Y   I N F O R M A T I O N  ══════════════
+import json
 
-{body}
+def fampay_format(raw):
+    d = json.loads(raw)
+
+    return f"""══════════════  F A M P A Y   I N F O R M A T I O N  ══════════════
+
+┃ 🔹 CONTACT DETAILS
+┃ 🆔 Fam ID      : {d.get('id','N/A')}
+┃ 👤 Name        : {d.get('name','N/A')}
+┃ 📞 Phone       : {d.get('phone','N/A')}
+┃ 📡 Source      : {d.get('source','N/A')}
+┃ ✅ Status      : {d.get('status','N/A')}
+┃ 🗂 Type        : {d.get('type','N/A')}
 
 ══════════════════════════════════
           BUY API - @SUBHXCOSMO
 ══════════════════════════════════
 ══════════════════════════════════
           MADE BY - @LingTech_Dev
-══════════════════════════════════
-"""
+══════════════════════════════════"""
