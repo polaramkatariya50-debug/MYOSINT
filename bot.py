a0defe7e0051ec20bb6fef22c8959795ae82e765
OSINT <polaramkatariya50@gmail.com>
2026-01-12 21:38:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 from pyrogram import Client
-from config import BOT_TOKEN
+from config import BOT_TOKEN, API_ID, API_HASH
 
 app = Client(
-    "osint-bot",
+    name="osint-bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
     bot_token=BOT_TOKEN,
     plugins=dict(root="handlers")
 )
 
+print("🤖 Bot starting...")
 app.run()
